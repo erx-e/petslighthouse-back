4b5951fdb7528cb76549fad8ac036bb53bef49b4
erox <figueroa0cristian@gmail.com>
2022-09-12 13:14:58 -0500

    edit date to utc

--- Validations/CustomValidations.cs
+++ Validations/CustomValidations.cs
@@ -6,6 +6,6 @@ namespace petsLighthouseAPI.Validations
     public class DateRangeAttribute : RangeAttribute
     {
         public DateRangeAttribute()
-          : base(typeof(DateTime), DateTime.Now.AddYears(-20).ToShortDateString(), DateTime.Now.ToShortDateString()) { }
+          : base(typeof(DateTime), DateTime.Now.AddYears(-20).ToShortDateString(), DateTime.UtcNow.ToString()) { }
     }
 }
